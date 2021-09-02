59d6a0b14e75f158915275f395cc7b0e54ec9363
ndmancini <ndmancini@gmail.com>
2021-09-02 10:23:55 -0300

    Primeros LWC

--- force-app/main/default/lwc/assignment/assignment.js
+++ force-app/main/default/lwc/assignment/assignment.js
@@ -1,11 +1,36 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import getPendingRoles from '@salesforce/apex/ProjectDataService.getPendingRoles';
+import getFreeResources from '@salesforce/apex/ProjectDataService.getFreeResources';
+import START_DATE_FIELD from '@salesforce/schema/Project__c.Start_Date__c';
+import END_DATE_FIELD from '@salesforce/schema/Project__c.End_Date__c';
 
 export default class Assignment extends LightningElement {
-    
-    @api recordId;
 
-    connectedCallback() {
-        console.log(this.recordId);
+    @api recordId;
+    roles;
+    freeResources;
+
+    @wire(getRecord, { recordId: '$recordId', fields: [START_DATE_FIELD, END_DATE_FIELD] })
+    project;
+
+    get startDate() {
+        return getFieldValue(this.project.data, START_DATE_FIELD)
     }
-    
+
+    get endDate() {
+        return getFieldValue(this.project.data, END_DATE_FIELD)
+    }
+
+    @wire(getPendingRoles, { projectId: '$recordId' })
+    pendindRoles(result) {
+        this.roles = result;
+        //console.log(this.roles.data);
+    }
+
+    @wire(getFreeResources, { startDate: '$startDate', endDate: '$endDate' })
+    freeResourcesMethod(result) {
+        this.freeResources = result;
+    }
+
 }
